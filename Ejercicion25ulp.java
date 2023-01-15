f4ab33c8d720551b04fcca16998722bdafc4871a
emifalcon <109958452+emifalcon@users.noreply.github.com>
2023-01-15 15:52:29 -0300

    Ejercicion25

---
+++ Ejercicion25ulp.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author usuario
+ */
+public class Ejercicion25ulp {
+    public static void main(String[] args) {
+        
+        int[][] nuevaMatriz = new int[matriz[0].length][matriz.length];
+		
+for (int x=0; x < matriz.length; x++) {
+  for (int y=0; y < matriz[x].length; y++) {
+    nuevaMatriz[y][x] = matriz[x][y];
+  }
+}
+        
+        
+        
+        
+    }
+}
