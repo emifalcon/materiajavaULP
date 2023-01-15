70c33a2fefcebeb4d7250ed48ed60d78ba59df51
emifalcon <109958452+emifalcon@users.noreply.github.com>
2023-01-15 14:17:45 -0300

    Ejercicio1extrapoo

---
+++ Ejercicion1Extrapoo.java
@@ -0,0 +1,35 @@
+/*
+ 
+1. Desarrollar una clase Cancion con los siguientes atributos: titulo y autor. Se deberá́
+definir además dos constructores: uno vacío que inicializa el titulo y el autor a cadenas
+vacías y otro que reciba como parámetros el titulo y el autor de la canción. Se deberán
+además definir los métodos getters y setters correspondientes
+
+ */
+package ejercicion1extrapoo;
+
+/**
+ *
+ * @author usuario
+ */
+public class Ejercicion1Extrapoo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
